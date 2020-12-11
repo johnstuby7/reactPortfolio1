f33f1437952d7caff9e4ab83bab574f6749cb240
jstuby <john.stuby@hiregroundsoftware.com>
2020-12-11 10:48:24 -0700

    Updates

---
+++ precache-manifest.ac2f9b21edf6e488cb644ab4941810cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a4157e75e8df1c96dd311124a2d6d91",
+    "url": "/reactPortfolio1/index.html"
+  },
+  {
+    "revision": "94a39f2d4a1f1f813272",
+    "url": "/reactPortfolio1/static/css/main.cecf7af3.chunk.css"
+  },
+  {
+    "revision": "060aab83dcae9be7115b",
+    "url": "/reactPortfolio1/static/js/2.2e45242d.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/reactPortfolio1/static/js/2.2e45242d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94a39f2d4a1f1f813272",
+    "url": "/reactPortfolio1/static/js/main.6c68d655.chunk.js"
+  },
+  {
+    "revision": "ade185662e2d6bd43a33",
+    "url": "/reactPortfolio1/static/js/runtime-main.735852fa.js"
+  },
+  {
+    "revision": "b0f39666248c59675b96ff62c0580aea",
+    "url": "/reactPortfolio1/static/media/JohnPhoto.b0f39666.jpg"
+  }
+]);
